82a5046a6e478bc304d3cce203cd05701efb5b24
Geampier Merge Teodoro <57604834+jeanmerje@users.noreply.github.com>
2026-07-07 22:29:20 -0500

    Update 02_App.py

--- 02_App.py
+++ 02_App.py
@@ -10,4 +10,10 @@ import streamlit as st
 st.title("Proyecto Fundamentals")
 st.sidebar.title("Parámetros")
 
-valor_inicial=st.number_input("Ingrese el valor inicial")
+valor_inicial=st.number_input("Ingrese el valor inicial",value=0)
+valor_final=st.number_input("Ingrese el valor final,value=1)
+
+lista_numerica=list(range(valor_inicial,valor_final))
+st.write(lista_numerica)
+
+                            
