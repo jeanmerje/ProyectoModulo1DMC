95abf5eb06b888561e47e937d07a6e2346e4f3b8
Geampier Merge Teodoro <57604834+jeanmerje@users.noreply.github.com>
2026-07-09 22:14:42 -0500

    Update 02_App.py

--- 02_App.py
+++ 02_App.py
@@ -6,13 +6,18 @@ Created on Thu Jul  2 22:20:43 2026
 """
 
 #CONECTANDO STREAMLIT:
-import streamlit as st 
+#Insertando librerias:
+import streamlit as st
+#Insertando textos iniciales:
 st.title("Proyecto Fundamentals")
 st.sidebar.title("Parámetros")
-
+#Insertando imagenes:
+st.image("Python_logo.png")
+st.sidebar.image("DMC.png")
+#Insertando algunos texbox
 valor_inicial=st.number_input("Ingrese el valor inicial",value=0)
 valor_final=st.number_input("Ingrese el valor final",value=1)
-
+#Mostrando la lista:
 lista_numerica=list(range(valor_inicial,valor_final))
 st.write(lista_numerica)
 
