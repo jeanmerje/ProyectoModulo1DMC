69744bcb64da440cdc86b54f4402551f505bc408
Geampier Merge Teodoro <57604834+jeanmerje@users.noreply.github.com>
2026-07-09 22:31:08 -0500

    Update 02_App.py

--- 02_App.py
+++ 02_App.py
@@ -19,6 +19,7 @@ st.sidebar.image("DMC.png")
 modulo=st.sidebar.selectbox("Elija un modulo",["Listas","Arrays","Funciones"])
 #Mostrando información
 if modulo=="Listas":
+  st.write("Estas en el modulo de listas")
   #Insertando algunos texbox
   valor_inicial=st.number_input("Ingrese el valor inicial",value=0)
   valor_final=st.number_input("Ingrese el valor final",value=1)
@@ -26,13 +27,15 @@ if modulo=="Listas":
   lista_numerica=list(range(valor_inicial,valor_final))
   st.write(lista_numerica)
 elif modulo=="Arrays":
-  #st.write("Estas en el modulo de arreglos")
+  st.write("Estas en el modulo de arreglos")
   #Insertando algunos texbox
-  limite_inferior=st.number_input("Ingrese el limite inferior",value=0)
-  limite_superior=st.number_input("Ingrese el limite superior",value=1)
+  limite_inferior=st.number_input("Ingrese el limite inferior",value=1000)
+  limite_superior=st.number_input("Ingrese el limite superior",value=2000)
   cantidad_datos=st.number_input("Ingrese el número de datos a crear",value=10)
   datos_produccion=np.random.randint(limite_inferior,limite_superior,cantidad_datos)
   st.write(datos_produccion)
+  st.write("La producción total es: ",np.sum(datos_produccion))
+  st.write("La producción promedio es: ",np.mean(datos_produccion))
 else: 
   st.write("Estas en el modulo de funciones")
                             
